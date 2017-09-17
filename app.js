062fc7f544004fc14121217f248dc8a63198dc65
Joseph Schoech <js@js-cpu.local>
2017-09-17 16:03:29 -0600

    second try

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 // compiles dependancies into build.js with browserify
 require('lazysizes');
 var $ = require('jquery');
+	
+// call the funtion once the image is loaded
+$(".hero img").on("load",function() {
 
-// call the lazy function when lazysizes changes the image class
-$('.hero').bind("DOMSubtreeModified", lazy);
+  // traverse to the next preloader element
+  var preloader = $(this).parent().next();
+	
+  // call the function when the animation finishes its current iteration
+  $(preloader).on('animationiteration webkitAnimationIteration', function(){
 
-function lazy(){
+    // pause the animation
+	$(this).css("animation-play-state", "paused");
 
-	// call the function when the animation finishes its current iteration
-	$(".preloader").one('animationiteration webkitAnimationIteration', function(){
+	// traverse to the relevant .hero element
+	var hero = $(this).prev();
+		
+	// add the fade-in class to fade the image in
+	$(hero).addClass("fade-in");
 
-		// pause the animation
-		$(this).css("animation-play-state", "paused");
+  }); 
 
-		// add the fade-in class to fade the image in
-		$(".hero").addClass("fade-in");
-	}); 
-}
+});	
